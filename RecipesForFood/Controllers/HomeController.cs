ebe21d7178334a1446266dabf147a280bd3ba6e3
Bill Enloe <billiamspub@yahoo.com>
2018-11-19 09:04:35 -0800

    Intro to controller, model, and views.

--- RecipesForFood/Controllers/HomeController.cs
+++ RecipesForFood/Controllers/HomeController.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipesForFood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RecipesForFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello from the HomeController!";
+            var model = new Recipe { Id = 1, Name = "Beans and Rice" };
+            return View(model);
         }
     }
 }
---
+++ RecipesForFood/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipesForFood.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
